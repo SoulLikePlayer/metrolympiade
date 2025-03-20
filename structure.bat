f84d6a5773196bd9b22ed4018a88ff74d5f84c8e
louis Lazare <louislazare.pro@gmail.com>
2025-03-20 21:59:16 +0100

    [INITIAL COMMIT]

---
+++ structure.bat
@@ -0,0 +1,14 @@
+src/
+├── App.vue  # Point d'entrée principal de l'application Vue.js
+├── main.js  # Initialise l'application et monte Vue
+├── api/  # Gestion des appels API
+├── assets/  # Ressources statiques (images, styles, etc.)
+│   ├── img/  # Images du projet
+│   ├── style/  # Fichiers CSS globaux
+│   │   └── style.css  # Style principal de l'application
+├── components/  # Composants réutilisables de l'application
+├── composables/  # Fonctions réutilisables sous forme de composables Vue
+├── layout/  # Composants de mise en page (Header, Footer, etc.)
+├── pages/  # Pages principales de l'application
+├── router/  # Configuration des routes avec Vue Router
+├── store/  # Gestion de l'état global (Vuex ou Pinia)
