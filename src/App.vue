07c61d811c67984fc9e30d8842d22f5d15209be4
louis Lazare <louislazare.pro@gmail.com>
2025-04-02 08:54:26 +0200

    [FEAT] : Ajout du header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <div id="app">
+    <Header />
     <RouterView />
   </div>
 </template>
 
 <script setup>
 import { RouterView } from 'vue-router';
+import Header from './components/Header.vue';
 </script>
---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <header class="header">
+    <nav class="nav">
+      <RouterLink to="/" class="nav-link">Accueil</RouterLink>
+      <RouterLink to="/login" class="nav-link">Connexion</RouterLink>
+    </nav>
+  </header>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+
+<style scoped>
+@import "../assets/style/Header.css";
+</style>
