743ab36fa2177af3a4caa3040dc3af677f985426
louis Lazare <louislazare.pro@gmail.com>
2025-04-02 08:41:20 +0200

    [FEAT] Addition of: -> a home page start
    -> a router
    
    -> the default style

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container homepage fade-in">
+    <section class="hero">
+      <h1>Bienvenue sur Metrolympiades</h1>
+      <p>Gérez facilement vos événements de team building sportif.</p>
+      <RouterLink to="/login" class="btn primary">Se connecter</RouterLink>
+    </section>
+  </div>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+
+<style scoped>
+@import "../assets/style/HomePage.css";
+</style>
